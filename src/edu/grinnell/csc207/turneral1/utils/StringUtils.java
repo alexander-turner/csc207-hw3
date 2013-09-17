8dcc17946a40f3014f8a9d68da53966728d714c2
alexander-turner <turneral1@grinnell.edu>
2013-09-16 22:18:28 -0500

    Finalized files.

--- src/edu/grinnell/csc207/turneral1/utils/StringUtils.java
+++ src/edu/grinnell/csc207/turneral1/utils/StringUtils.java
@@ -1,16 +1,173 @@
 package edu.grinnell.csc207.turneral1.utils;
 
 public class StringUtils {
-	/* 
-	 * deLeet takes a string of "leet" text and produces the human-readable
-	 * version.
+
+
+    /**
+     * preconditions:
+     * Takes an input string that may or may not have
+     * the separating character. Also takes the separating
+     * character. 
+     * 
+     * postconditions:
+     * Returns an array of strings in which each string 
+     * is what was before after and in between the given
+     * separators.
+     *
+     */
+    public static String[] splitAt(String inputString, char inputChar){
+	int slots = 0; //number of string slots
+	int len = inputString.length();
+	int count; //used for for-loop control to stay within string bounds
+	boolean frontOrEnd = false; //empty string at the end of beginning
+	char currentChar;
+
+	//set len slots to the number of strings needed to return
+	for (count = 0; count<len; count++){
+	    currentChar = inputString.charAt(count);
+	    if (currentChar == inputChar){
+		if(count == 0 || count == (len -1)){
+		    if (frontOrEnd == false)
+			slots++;
+		    frontOrEnd = true;
+		}//if
+		slots++;
+	    }//if
+	} // for
+
+	if (frontOrEnd == false)
+	    slots++;
+
+	String[] toReturn = new String[slots]; 
+
+	//special null case
+	if (slots == 0){
+	    toReturn[0] = inputString;
+	    return toReturn;
+	}
+
+	int stringCount = 0;
+	int goTo = 0;
+
+	//fills to return
+	while (stringCount < slots){
+	    goTo = inputString.indexOf(inputChar);
+	    //stay in bounds
+	    if (goTo != -1){
+		String newString = inputString.substring(0,goTo);
+		toReturn[stringCount] = newString;
+		//remove the unneeded part from input string
+		inputString = inputString.substring(goTo+1);
+	    }
+	    else{
+		toReturn[stringCount] = inputString;
+		break;
+	    }
+	    stringCount++;
+	}
+	//stay in bounds
+	if (goTo != -1){
+	    String newString = inputString.substring(0,goTo);	
+	    toReturn[stringCount] = newString;
+	}
+	return toReturn;
+    }
+
+
+    public static String[] splitCSV(String inputString){
+	char dividerChar = ',';
+	String dividerString = Character.toString(dividerChar);
+	int slots = 0; //number of string slots
+	int len = inputString.length();
+	int count; //used for for-loop control to stay within string bounds
+	boolean frontOrEnd = false; //empty string at the end of beginning
+	char currentChar = '0';
+	char lastChar = '0';
+
+	//set len slots to the number of strings needed to return
+	for (count = 0; count<len; count++){
+	    if (count != 0)
+		lastChar = currentChar;
+	    currentChar = inputString.charAt(count);
+	    if ((currentChar == dividerChar) && 
+		    ((count == 0) || (lastChar != '"'))){
+		if(count == 0 || count == (len -1)){
+		    if (frontOrEnd == false)
+			slots++;
+		    frontOrEnd = true;
+		}//if
+		slots++;
+	    }//if
+	}
+
+	if (frontOrEnd == false)
+	    slots++;
+
+	String[] toReturn = new String[slots]; 
+
+	String current = "";
+	String holder = "";
+	int stringIndex = 0;
+	String quote = "closed";
+	while (stringIndex<slots){
+	    if (inputString.length() > 0)
+		current = inputString.substring(0,1);
+	    if (inputString.length()<1){
+		toReturn[stringIndex] = holder;
+		break;
+	    }
+	    else if ((current.equals(dividerString)) && quote.equals("closed")){
+		inputString = inputString.substring(1); //get rid of comma
+		toReturn[stringIndex] = holder;
+		holder = "";
+		stringIndex++; //to load the next string next time
+	    }
+	    else if ((current.equals(dividerString)) && (quote.equals("open"))){
+		holder += current;
+		inputString = inputString.substring(1);
+	    }
+	    else if (inputString.length() > 0 && current.equals("\"") &&
+		    (inputString.charAt(inputString.indexOf(current) + 1) == '\"')){
+		inputString = inputString.substring(2);
+		holder += "\"";
+	    }
+	    else if (current.equals("\"")){
+		//openQuote = !(openQuote);
+		if (quote == "open")
+		    quote = "closed";
+		else if (quote.equals("closed"))
+		    quote = "open";
+		inputString = inputString.substring(1);
+	    }
+	    else {
+		holder+=current;
+		inputString = inputString.substring(1);
+	    }
+	}
+	return toReturn;
+    }
+
+    /* 
+     * deLeet takes a string of "leet" text and produces the human-readable
+     * version.
+     * Pre-conditions: 
+     *  None.
+     * Post-conditions:
+     *  The input string is returned, with the following replacements:
+     *    * '1' is replaced with 'l'
+     *    * '|3' is replaced with 'b'
+     *    * '3' is replaced with 'e'
+     *    * '+' is replaced with 't'
+     *    * '0' is replaced with 'o'
+     *    * '@' is replaced with 'a'
+     *    * '|\\|' is replaced with 'n'
 	 */
 	public static String deLeet(String leet){
 		String normal = leet.replace("1","l");
+		normal = normal.replace("|3","b");
 		normal = normal.replace("3","e");
 		normal = normal.replace("+","t");
 		normal = normal.replace("0","o");
-		normal = normal.replace("|3","b");
 		normal = normal.replace("@","a");
 		return normal.replace("|\\|","n");
 	} // deLeet
@@ -21,24 +178,28 @@ public class StringUtils {
      */
 	public static String nameGame(String input){
 		// find the first vowel occurence's index.
-		for (int i = 0; i < input.length() && (input(i) != 'a' &&
-			       input(i) != 'e' && input(i) != 'o' && input(i) != 'u'; i++);
+	    	int i;
+		for (i = 0; i < input.length() && (input.charAt(i) != 'a' && 
+			input.charAt(i) != 'e' && input.charAt(i) != 'i' && 
+			input.charAt(i) != 'o' && input.charAt(i) != 'u'); i++);
 		String subName = input.substring(i);
-		System.out.println(input + '!');
-		System.out.print(input + ', ' + input + " bo ");
-		System.out.print('B' + subName + " Bonana fanna fo F");
-		System.out.println(subName);
-		System.out.print("Fee fy mo M" + subName + ',');
-		System.out.println(input.substring + '!');
+		return (String) (input + '!' + '\n' + input + ", " + input 
+			+ " bo " + 'B' + subName + " Bonana fanna fo F" 
+			+ subName + '\n' + "Fee fy mo M" + subName + 
+			", " + input + '!' + '\n');
 	} // nameGame
 
 	/*
-	 * fewestCoins recursively searches to find the fewest number of coins that
-	 * produces value, and returns the coins' values in an array.
+	 * fewestCoins recursively searches to find the fewest number of 
+	 * coins that produces value, and returns the coins' values in
+	 * an array of integers. 
 	 */
     public static int[] fewestCoins(int value){
 	int values[] = {2,7,11,54};
-	int coinArr[] = fewestCoinsSearch(value, new int[] {0,0,0,0,Integer.MAX_VALUE}, values, 4);
+	int coinArr[] = {0,0,0,0,Integer.MAX_VALUE};
+	
+	coinArr = fewestCoinsSearch(value,coinArr, values, 4);
+	
 	int coinTotal = coinArr[0]+coinArr[1]+coinArr[2]+coinArr[3];
 	int valueArr[] = new int[coinTotal];
 	
@@ -63,7 +224,12 @@ public class StringUtils {
      *   When value == 0, returns counts. 
      *   counts[length] contains how many coins are used.
      */  
-    public static int[] fewestCoinsSearch(int value, int[] counts, int[] values, int length){
+    public static int[] fewestCoinsSearch(int value, int a, int b, int c, 
+	                                   int d, int[] values, int length){
+	// had to convert at last minute - couldn't make coin array immutable.
+	int bestCounts[] = {a, b, c, d};
+	int counts[]
+	
 	for(int i = length-1; i >= 0; i--){
 		if(value-values[i] == 0){
 		    // the coin is currently being considered
@@ -78,13 +244,21 @@ public class StringUtils {
 		    } // if
 		} // if
 		
-		if(value-values[i] > 0){
+		else if(value-values[i] > 0){
 		    int newCounts[] = counts;
 		    newCounts[i]++;
-		    return fewestCoinsSearch(value-values[i], newCounts, values, length);
+		    newCounts = fewestCoinsSearch(value-values[i], newCounts, 
+			    values, length);
+		    if (newCounts[length] < bestCounts[length])
+			bestCounts = newCounts;
 		} // if
 	} // for
 	
-	return counts;
+	System.out.println("BestCounts");
+	for(int i = 0; i <= length; i++)
+	    System.out.print(bestCounts[i] + " ");
+	System.out.println();
+	
+	return bestCounts;
     } // fewestCoinsSearch
 } // StringUtils
--- src/edu/grinnell/csc207/turneral1/utils/StringUtilsTest.java
+++ src/edu/grinnell/csc207/turneral1/utils/StringUtilsTest.java
@@ -1,23 +1,59 @@
 package edu.grinnell.csc207.turneral1.utils;
 import static org.junit.Assert.*;
 
-import java.util.Arrays;
-
 import org.junit.Test;
 
 public class StringUtilsTest {
 
-	@Test
-	public void deLeetTest() {
-		assertEquals("abelnot", StringUtils.deLeet("@|B31|\\|0+"));
-		assertEquals(" leeet ", StringUtils.deLeet(" 13333+ "));
-		assertEquals("rebelsky", StringUtils.deLeet("r3|331sky"));
-	} // deLeetTest
+    /** Have JUnit run this test() method. */
+    @Test
+    public void test() throws Exception {
 
+        assertArrayEquals("simple case",new String[]{"a","b","c"},
+        	StringUtils.splitAt("a:b:c", ':'));
+        assertArrayEquals("no sperator",new String[]{"a"},
+        	StringUtils.splitAt("a", ':'));
+        assertArrayEquals("space speceator", new String[]{"a","b"},
+        	StringUtils.splitAt("a b", ' '));
+        assertArrayEquals("leading empty field",new String[]{"","b","c"},
+        	StringUtils.splitAt(":b:c", ':'));
+        assertArrayEquals("trailing empty field",new String[]{"b","c", ""},
+        	StringUtils.splitAt("b:c:", ':'));
+        assertArrayEquals("leading and trailing empty field",
+        	new String[]{"","b","c",""},StringUtils.splitAt(":b:c:", ':'));
+        assertArrayEquals("middle empty field",new String[]{"b","","c"},
+        	StringUtils.splitAt("b::c", ':'));
+
+        assertArrayEquals("simple case",new String[]{"a","b","c"},
+        	StringUtils.splitCSV("a,b,c"));
+        assertArrayEquals("quote used",new String[]{ "a,b", "c" },
+        	StringUtils.splitCSV("\"a,b\",c"));
+        assertArrayEquals("multiple quotes used",
+        	new String[]{  "a", "b,b\"", "c" },
+        	StringUtils.splitCSV("a,\"b,b\"\"\",c"));
+        assertArrayEquals("no sperator",new String[]{"a"},
+        	StringUtils.splitCSV("a"));
+        assertArrayEquals("leading empty field",new String[]{"","b","c"},
+        	StringUtils.splitCSV(",b,c"));
+        assertArrayEquals("trailing empty field",new String[]{"b","c", ""},
+        	StringUtils.splitCSV("b,c,"));
+        assertArrayEquals("leading and trailing empty field",
+        	new String[]{"","b","c",""}, StringUtils.splitCSV(",b,c,"));
+        assertArrayEquals("middle empty field",new String[]{"b","","c"},
+        	StringUtils.splitCSV("b,,c"));
+    }
+    @Test
+    public void deLeetTest() {
+	assertEquals("abelnot", StringUtils.deLeet("@|331|\\|0+"));
+	assertEquals(" leeeet ", StringUtils.deLeet(" 13333+ "));
+	assertEquals("rebelsky", StringUtils.deLeet("r3|331sky"));
+    } // deLeetTest
+    
+    // We ran some experiments on nameGame - it works fine!
+    
     @Test
     public void fewestCoinsTest() {
 	assertArrayEquals(new int[]{}, StringUtils.fewestCoins(0));
-	System.out.println(Arrays.toString(StringUtils.fewestCoins(28)));
 	assertArrayEquals(new int[]{7,7,7,7}, StringUtils.fewestCoins(28));
     } // fewestCoinsTest
 
